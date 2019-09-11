9a541470cb75fee712299b07dcd3631996189afd
github username <github email address>
2019-09-11 11:35:16 -0400

    done

--- src/App.js
+++ src/App.js
@@ -5,11 +5,57 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  state = {
+    books: [],
+    bookShelf: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3005/books")
+    .then(resp => resp.json())
+    .then(books => this.setState({books}))
+  }
+
+  handleClickedBook = (book) => {
+    if (this.state.bookShelf.includes(book)) {
+      return;
+    }
+    this.setState({
+      bookShelf: [...this.state.bookShelf, book]
+    })
+    
+  }
+
+  removeBookFromShelf = (bookOnShelf) => {
+    const updatedBookShelf = this.state.bookShelf.filter(book => {
+      return book !== bookOnShelf
+    })
+    this.setState({
+      bookShelf: updatedBookShelf
+    })
+  }
+
+  handleSubmitForm = (newBook) => {
+    this.setState({
+      books: [...this.state.books, newBook]
+    })
+    
+  }
+  
   render() {
+    console.log(this.state.bookShelf);
+    
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList
+        handleSubmitForm={this.handleSubmitForm}
+        handleClickedBook={this.handleClickedBook}
+         books={this.state.books}/>
+        <Bookshelf 
+        handleClickedBook={this.removeBookFromShelf}
+        books={this.state.bookShelf}
+        />
       </div>
     );
   }
--- src/components/Book.js
+++ src/components/Book.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const Book = props => {
   return (
-    <div>
-      <h2>{/*book title*/}</h2>
-      {/*book img*/}
+    <div onClick={() => props.handleClickedBook(props.book)}>
+      <img src={props.book.img}/>
+      <h2>{props.book.title}</h2>
+      
     </div>
   );
 };
