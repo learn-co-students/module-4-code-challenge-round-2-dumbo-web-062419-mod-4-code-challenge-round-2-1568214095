6d5018304871565bcc6faf10045e56eb4870aa5d
github username <github email address>
2019-09-11 12:06:27 -0400

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,38 @@
 import React from "react";
 
 class Form extends React.Component {
+  state ={
+    title: '',
+    author: '',
+    img: ''
+  }
+
+  titleChange = (e) => {
+    this.setState({ title: e.target.value})
+  }
+
+  authorChange = (e) => {
+    this.setState({ author: e.target.value})
+  }
+
+  imgChange = (e) => {
+    this.setState({ img: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.newBook(this.state)
+  }
+
   render() {
-    return <h1>{/*create form*/}</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        title<input onChange={this.titleChange} type="text" name="title" value={this.state.title} />
+        author<input onChange={this.authorChange} type="text" name="author" value={this.state.author}/>
+        img<input onChange={this.imgChange} type="text" name="img" value={this.state.img}/>
+        <input type="submit"  />
+      </form>
+    );
   }
 }
 
